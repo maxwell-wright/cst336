6e222a3db53fa8ee59833fd341c55af4c7538e38
Maxwell Wright <mleonciowright@csumb.edu>
2019-04-30 02:14:36 +0000

    fixes to hw4

--- homework/hw4/api/releaseAPI.php
+++ homework/hw4/api/releaseAPI.php
@@ -4,10 +4,11 @@
 include '../../../inc/dbConnection.php';
 $conn = getDatabaseConnection("pokeDatabase");
 
-$pokeId = $_GET['id'];
+$pokeId = $_GET['item'];
 
-$sql = "DELETE FROM `poke_party` WHERE id=$pokeId";
-$stmt = $conn->prepare($sql);
+// STILL HAVING ISSUES WITHOUT THIS SQL STATEMENT....BUT CAN'T FIGURE OUT WHAT MY DB DOESN'T LIKE
+$sql = "DELETE FROM `poke_party` WHERE `id` = $pokeId"; 
+$stmt = $conn->prepare($sql); 
 $stmt->execute();
 
 echo json_encode($pokeId);
