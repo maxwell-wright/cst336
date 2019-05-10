976554e2cebda614d037dece52b7553bc023228c
Maxwell Wright <mleonciowright@csumb.edu>
2019-05-10 01:01:56 +0000

    uhhh

---
+++ Project/delete.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['adminName'])){
+    exit;
+}
+
+include 'api/inc/dbConnection.php';
+
+$conn = getDatabaseConnection("junkyard");
+
+$sql = "DELETE FROM `jy_product` WHERE `jy_product`.`productId` = " . $_POST['productId'];
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+$sql = "DELETE from `jy_cart` WHERE `jy_cart`.`pId` = " . $_POST['productId'];
+
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+//echo $sql;
+
+header("Location: admin.php");
+
+//should work
+?>
